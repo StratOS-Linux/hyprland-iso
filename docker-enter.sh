9e71e04dc79d2b6b1e1e94340e9d37e5a948d947
zstg <o0vckutt@duck.com>
2025-12-23 21:44:44 +0530

    Updated profiledef.sh (removed warnings) and docker-enter.sh to pull from ghcr

--- docker-enter.sh
+++ docker-enter.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run --user root -w /iso --rm -it --privileged --mount type=bind,source=/proc,target=/proc -v ./:/iso stratos_arch_base
+docker pull ghcr.io/stratos-linux/stratos-base:latest
+docker run --user root -w /iso --rm -it --privileged --mount type=bind,source=/proc,target=/proc -v ./:/iso ghcr.io/stratos-linux/stratos-base:latest
